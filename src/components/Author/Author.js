e0fa24a0604c1a571f33e2796f4feac0ff61c64c
Karlygachbakytova <karlygach.bakytova@mail.ru>
2021-04-06 06:33:35 +0600

    create useEffect and import it

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -1,11 +1,22 @@
 
 import classes from "./Author.module.css";
 const Author = ({userId}) => {
+ 
+    const [email, setEmail] = useState("Loading")
+  
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users/' + [userId]
+          .then((response) => {
+            console.log(response);
+            const email = response.data.email;
+            setEmail(email);
+          });
+      }, [userId]);
 
 
     return ( 
-      <div>
-          
+      <div className={classes.Author}>
+{email}
       </div>
      );
 }
